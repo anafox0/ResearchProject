54d3bed022e3e6bba57167ba86e4ab8df3fb93e7
anafox0 <b9066333@ncl.ac.uk>
2022-02-04 15:44:55 +0000

    Initial attempt at creating function to pull the correct data No age filter yet applied

---
+++ Real pull data function.R
@@ -0,0 +1,66 @@
+# Creating a singular function for data extraction of:
+# Clinical data
+# Transcriptomic/expression data
+# Mutational data
+
+real.pull.data <- function(
+  project = "",
+  tumor = "" ,
+  workflow.type = "HTSeq - FPKM-UQ",
+  save.filename = ""
+){
+  # Downloading expression data
+  
+  query.exp <- GDCquery(
+    project = project,
+    data.category = "Transcriptome Profiling",
+    legacy = FALSE,
+    data.type = "Gene Expression Quantification",
+    workflow.type = workflow.type
+  )
+  
+  GDCdownload(query.exp)
+  expression <- GDCprepare(query = query.exp,
+                           save = TRUE,
+                           save.filename = save.filename)
+  
+  
+  # Downloading clinical data
+  
+  query.clin <- GDCquery(
+    project = project,
+    file.type = "xml",
+    data.category = "Clinical",
+    legacy = FALSE,
+    data.type = "Clinical Supplement"
+  )
+  
+  GDCdownload(query.clin)
+  clinical <- GDCprepare_clinic(query = query.clin, clinical.info = "patient")
+  
+
+  # Downloading mutational data
+  
+  library(maftools)
+  
+  mutational <- GDCquery_Maf(tumor = tumor, pipelines = "mutect2")
+
+  return(list(expression = expression,
+              clinical = clinical,
+              mutational = mutational))
+  
+  
+}
+
+
+lgg.data <- real.pull.data(project = "TCGA-LGG",
+                           tumor = "LGG",
+                           save.filename = "LGG.Exp.rda"
+)
+
+
+
+
+
+
+
